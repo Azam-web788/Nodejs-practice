87facb66f0081eee9d212f9af0b08b0b26398321
Azam-web788 <azamkhan505560@gmail.com>
2026-05-26 12:19:15 +0500

    nodejs practice

---
+++ app.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+
+// fs.writeFile("index.txt" , "Hello world" , (err) => {
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log("file created successfully");
+        
+//     }
+// })
+
+// fs.appendFile("index.txt" , " How are you" , (err) => {
+//     if (err) {
+//         console.error(err);
+//     }else {
+//         console.log("data appended succcessfully");
+        
+//     }
+// })
+// fs.appendFile("index.txt" , " Alhamdulillah" , (err) => {
+//     if (err) {
+//         console.error(err);
+//     }else{
+//         console.log("data appended successfully");
+        
+//     }
+// })
+
+// fs.rename("index.txt" , "hello.txt" , (err) => {
+//     if (err) {
+//         console.error(err);
+//     }else {
+//         console.log("file renamed successfully");
+//     }
+// })
+
+// fs.copyFile("./hello.txt" , "./copy/copy.txt" , (err) => {
+//     if (err) {
+//         console.error(err);
+//     }else {
+//         console.log("file copied successfully");
+        
+//     }
+// })
+
+// fs.unlink("./hello.txt" , (err) => {
+//     if (err) {
+//         console.error(err);
+//     }else{
+//         console.log("file deleted successfully");
+        
+//     }
+// })
+
+// fs.rmdir("./hello" , (err) => {
+//     if (err) {
+//         console.error(err.message);
+//     }else {
+//         console.log("directory removed successfully");
+        
+//     }
+// })
+
+// fs.writeFile("hello.txt" , "Hello how are you" , (err) => {
+//     if (err) {
+//         console.error(err.message);
+//     }else{
+//         console.log("file created successfully");
+        
+//     }
+// })
+
+fs.appendFile("hello.txt" , " Alhamdullilah" , (err) => {
+    if (err) {
+        console.error(err.message);
+    }else{
+        console.log("data add successfully");
+        
+    }
+})
+
+fs.copyFile("hello.")
